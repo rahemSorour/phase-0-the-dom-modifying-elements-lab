3d842551306fbb2a60b15d6132ca41ff835abd50
rahem sorour <sorourrahem17@gmail.com>
2023-08-24 18:04:06 +0100

    completed assignemnt

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+const mainid=document.querySelector('main')
+
+mainid.remove()
+
+const newHeader=document.createElement('h1')
+
+newHeader.id = 'victory';
+
+newHeader.textContent = "is the champion";
+
+
+
+
+
+
